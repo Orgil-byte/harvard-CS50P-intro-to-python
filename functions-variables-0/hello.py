cdd439ee4f847c7b11f6dd8f08ec37d978cd0e08
Orgil-byte <ulziitogtohorgil1908@gmail.com>
2026-07-28 18:37:41 +0800

    String methods done

--- functions-variables-0/hello.py
+++ functions-variables-0/hello.py
@@ -1,7 +1,24 @@
 #Ner asuugaad hariuldag programm
 
 #Ner asuuna
-name = input("What's your name?: ")
+name = input("What's your name?: ").strip().title()
+firstname, lastname = name.split()
 
-#Ner awaad hariulna.
-print(f"hello {name}!")
+# #Result=hello, name!
+# print(f"hello, {name}!")
+
+# print("hello, "+ name +"!")
+
+# print("hello,", end=" ")
+# print(f'{name}!')
+
+# print("hello,", name, "!" ,sep=' ')
+
+# #Result=hello, name's "Dog"
+
+# print(f"hello, {name}'s \"Dog\"!")
+
+# #Final result using all of it.
+
+print(f"Hello, sir {lastname} or rather {firstname}. It seems like you're dog \"DIED\"!!!", end=" OR ")
+print(f"Who gives a shit about your fcking dog {name}")
