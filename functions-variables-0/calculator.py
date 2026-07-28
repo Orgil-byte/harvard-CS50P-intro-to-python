d71e8398522744059cf17c51ae77267bfd2d6419
Orgil-byte <ulziitogtohorgil1908@gmail.com>
2026-07-28 20:12:58 +0800

    integers

---
+++ functions-variables-0/calculator.py
@@ -0,0 +1,7 @@
+firstNumber = float(input("First number: "))
+secondNumber = float(input("Last number: "))
+
+print(f"{firstNumber} + {secondNumber} = {round(firstNumber+ secondNumber):,}")
+
+print(f"{firstNumber} / {secondNumber}= {round(firstNumber/secondNumber, 2)}")
+print(f"{firstNumber} / {secondNumber}= {firstNumber/secondNumber:.2f}")
